8e7a046974a62346905d43f80334e0f6fb8f6ffb
sa <as>
2022-12-04 21:13:11 +0300

    func

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,10 +1,6 @@
 ﻿// Напишите программу, которая принимает на вход число (N) 
 // и выдаёт таблицу кубов чисел от 1 до N.
-
-Console.WriteLine("Введите число");
-int numb = Convert.ToInt32(Console.ReadLine());
-if (numb<=0) Console.WriteLine("Нужно ввести числа от 1 до N");
-else
+void Cube(int A);
 {
     Console.WriteLine();
     Console.WriteLine("Кубы чисел");
@@ -14,3 +10,9 @@ else
     Console.WriteLine(numbers);
     }
 }
+Console.WriteLine("Введите число");
+int numb = Convert.ToInt32(Console.ReadLine());
+if (numb<=0) Console.WriteLine("Нужно ввести числа от 1 до N");
+else 
+Cube(numb);
+
